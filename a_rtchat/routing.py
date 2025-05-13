fc123a127bf1e0ac5f8adfeecd81a4ce472fa640
htviet2004 <htviet4961@gmail.com>
2025-05-13 22:19:01 +0700

    Fix line endings

--- a_rtchat/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, re_path
-from .consumers import ChatroomConsumer, PrivateChatConsumer
-
-websocket_urlpatterns = [
-    path("ws/chatroom/<chatroom_name>/", ChatroomConsumer.as_asgi()),
-    re_path(r"ws/private-chat/(?P<other_username>\w+)/$", PrivateChatConsumer.as_asgi()),
-]
